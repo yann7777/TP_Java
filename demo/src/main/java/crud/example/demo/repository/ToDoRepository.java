f48a9263d112046579026f9da51a8ad7bf5b9111
yannmazanga78 <yannmazanga78@gmail.com>
2025-02-24 02:14:58 +0000

    Spring boot avec Swagger1

--- demo/src/main/java/crud/example/demo/repository/ToDoRepository.java
+++
@@ -1,9 +0,0 @@
-package crud.example.demo.repository;
-
-import crud.example.demo.model.ToDo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-public interface ToDoRepository extends JpaRepository<ToDo, Long>{
-    boolean existsByTitre(String titre);
-}
